8f1ef9fd2701b6edc154d69787fe3607d27cb066
Douglas Boyle <douglas@maisonboyle.com>
2022-01-19 20:45:32 +0000

    Add additional DTM/WLD tables meant for extra compression, using 1/2-ply search and uneven DTM table

---
+++ TidyTable/Tables/DTMTable.cs
@@ -0,0 +1,176 @@
+﻿using Chessington.GameEngine;
+using Chessington.GameEngine.AI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TidyTable.TableFormats;
+
+namespace TidyTable.Tables
+{
+    public class DTMTable // TODO: Create this, only storing DTM for white and using sub-DTM tables/small search to lookup
+    {
+        public string Classification;
+
+        private BoardNormaliser normalise;
+        private IndexGetter getIndex;
+        private WLDTable WLDTable;
+        private sbyte[] Data;
+
+        private int maxBits = 0;
+
+        public DTMTable(
+            WLDTable wldTable, // TODO: Having both of these is redundant
+            SolvingTable table 
+        )
+        {
+            Classification = table.Classification;
+            normalise = table.NormaliseBoard;
+            getIndex = table.GetIndex;
+
+            WLDTable = wldTable;
+            Data = new sbyte[table.MaxIndex]; // only stores values for white
+            for (int i = 0; i < table.WhiteTable.Length; i++)
+            {
+                var dtm = table.WhiteTable[i]?.DTM ?? 0;
+                if (table.WhiteTable[i]?.Outcome == Outcome.Draw)
+                {
+                    dtm = 0;
+                }
+                maxBits = Math.Max(dtm, maxBits);
+                Data[i] = dtm;
+            }
+            maxBits = (int)Math.Floor(Math.Log2(maxBits)) + 1;
+        }
+
+        public int DTM(in Board board)
+        {
+            // Probably unnecessary
+            var copy = new Board(board);
+            if (board.CurrentPlayer == Player.White)
+            {
+                normalise(copy);
+                return Data[getIndex(copy)];
+            } else
+            {
+                // DTM values not stored for black
+                // For a draw, return 0
+                // For a win, return 0 if no moves else minimum of DTM for white after next move + 1
+                // For a loss, return 0 if no moves else maximum of DTM for white after next move + 1
+                var outcome = WLDTable.GetOutcome(board);
+                if (outcome == Outcome.Draw) return 0;
+
+                var availableMoves = board.GetAllAvailableMoves();
+                if (availableMoves.Count == 0) return 0;
+
+                var gameInfo = new GameExtraInfo(board);
+                if (outcome == Outcome.Win)
+                {
+                    return availableMoves.Select(move => DTMForMove(copy, gameInfo, move)).Min();
+                } else
+                {
+                    return availableMoves.Select(move => DTMForMove(copy, gameInfo, move)).Max();
+                }
+            }
+        }
+
+        // DTM potentially requires a 1-ply search, and this therefore requires a 1 to 2-ply search
+        // Applies the same trick as above of min/maximising the DTM, doing a self-call for the lookup
+        public Move? GetMove(in Board board)
+        {
+            // Not actually necessary
+            var copy = new Board(board);
+            // Will rely on MakeMoveWithoutRecording/UndoMove move not modifying board
+            var moves = copy.GetAllAvailableMoves();
+            if (moves.Count == 0) return null;
+
+            var gameInfo = new GameExtraInfo(copy);
+            // also relies on GetOutcome not modifying board
+            switch (WLDTable.GetOutcome(copy))
+            {
+                case Outcome.Win:
+                    return moves.MinBy(move => DTMForMove(copy, gameInfo, move));
+                case Outcome.Lose:
+                    return moves.MaxBy(move => DTMForMove(copy, gameInfo, move));
+                default:
+                    // DTMForMove always adds 1, and draws have DTM = 0, so look for first with DTMForMove 1
+                    // Note: Drawn position, so no move leads to an immediate checkmate (other case where DTM = 0),
+                    //      and cannot checkmate self on own turn, so any loses would take at least 2 ply => DTM > 1
+                    return moves.First(move => DTMForMove(copy, gameInfo, move) == 1);
+            }
+        }
+
+        private int DTMForMove(Board board, GameExtraInfo gameInfo, Move move)
+        {
+            // Relies on this method not modifying (normalising) the object passed to it
+            board.MakeMoveWithoutRecording(move);
+            var dtm = DTM(board);
+            board.UndoMove(move, gameInfo);
+            return dtm + 1;
+        }
+
+        public void WriteToFile(string filename)
+        {
+             // As usual, write starting from LSB
+            int buffer = 0;
+            int bufferLength = 0;
+
+            using FileStream fs = File.OpenWrite(filename);
+            foreach (var value in Data)
+            {
+                buffer |= value << bufferLength;
+                bufferLength += maxBits;
+
+                // relies on maxBits <= 8, so only ever need to clear 1 byte of space
+                if (bufferLength >= 8)
+                {
+                    fs.WriteByte((byte)buffer);
+                    buffer >>= 8;
+                    bufferLength -= 8;
+                }
+            }
+            if (bufferLength > 0) fs.WriteByte((byte)buffer);
+        }
+
+        public DTMTable(
+            string filename,
+            string classification,
+            uint maxIndex,
+            IndexGetter getIndex,
+            BoardNormaliser normaliseBoard,
+            int maxBits // number of bits needed to store maximum DTM present in the table
+        )
+        {
+            if (!File.Exists(filename)) throw new FileNotFoundException(filename);
+
+            Classification = classification;
+            normalise = normaliseBoard;
+            this.getIndex = getIndex;
+            this.maxBits = maxBits;
+            sbyte bitMask = (sbyte)((1 << maxBits) - 1); // for extracting data from bottom of buffer
+            Data = new sbyte[maxIndex];
+
+            // As usual, write starting from LSB
+            int dataIndex = 0;
+            int buffer = 0;
+            int bufferLength = 0;
+
+            using var stream = File.Open(filename, FileMode.Open);
+            while (dataIndex < maxIndex)
+            {
+                // enough bits to read out an entry
+                if (bufferLength >= maxBits)
+                {
+                    Data[dataIndex++] = (sbyte)(buffer & bitMask);
+                    buffer >>= maxBits;
+                    bufferLength -= maxBits;
+                } else // read in another byte of data
+                {
+                    buffer |= stream.ReadByte() << bufferLength;
+                    bufferLength += 8;
+                }
+            }
+        }
+    }
+}
---
+++ TidyTable/Tables/WLDTable.cs
@@ -0,0 +1,91 @@
+﻿using Chessington.GameEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TidyTable.TableFormats;
+
+namespace TidyTable.Tables
+{
+    public class WLDTable
+    {
+        public string Classification;
+
+        // Based on TableEntry.Outcome, so 0 = draw, 1 = win, 2 = loss
+        // Stores all values for white, followed by all values for black
+        // As usual, fill each byte from the LSB upwards
+        public byte[] Outcomes;
+        public uint MaxIndex;
+
+        private byte buffer = 0;
+        private int bufferLength = 0;
+        private int index = 0;
+
+        private BoardNormaliser normalise;
+        private IndexGetter getIndex;
+
+        // Need SolvingTable, as only it exposes underlying white/black tables and max index, normalisation, indexing
+        public WLDTable(SolvingTable table)
+        {
+            Classification = table.Classification;
+            MaxIndex = table.MaxIndex;
+            normalise = table.NormaliseBoard;
+            getIndex = table.GetIndex;
+
+            // bytes = entries * 2 sides * 2 bits per entry / 8 bits per byte = entries / 2 (rounded up)
+            uint numBytes = (MaxIndex + 1) / 2;
+            Outcomes = new byte[numBytes];
+
+            for (var player = (int)Player.White; player <= (int)Player.Black; player++)
+            {
+                var colourTable = player == (int)Player.White ? table.WhiteTable : table.BlackTable;
+                Array.ForEach(colourTable, entry => WriteOutcome(entry?.Outcome ?? Outcome.Draw));
+            }
+
+            if (bufferLength > 0) Outcomes[index] = buffer;
+        }
+
+        public Outcome GetOutcome(in Board board)
+        {
+            // Probably unnecessary
+            var copy = new Board(board);
+            normalise(copy);
+            var index = getIndex(copy);
+            if (board.CurrentPlayer == Player.Black) index += MaxIndex;
+
+            int offset = (int)((index & 3) << 1); // 4 entries per byte = last 2 bits, 2 bit size = left shift
+            var b = Outcomes[index >> 2]; // 4 entries per byte = divide by 4
+            return (Outcome)((b >> offset) & 3);
+        }
+
+        private void WriteOutcome(Outcome outcome)
+        {
+            buffer |= (byte)((byte)outcome << bufferLength);
+            bufferLength += 2;
+            if (bufferLength == 8)
+            {
+                Outcomes[index++] = buffer;
+                buffer = 0;
+                bufferLength = 0;
+            }
+        }
+
+        public void WriteToFile(string filename) => File.WriteAllBytes(filename, Outcomes);
+
+        public WLDTable(
+            string filename,
+            string classification,
+            uint maxIndex,
+            IndexGetter getIndex,
+            BoardNormaliser normaliseBoard
+        )
+        {
+            Classification = classification;
+            MaxIndex = maxIndex;
+            normalise = normaliseBoard;
+            this.getIndex = getIndex;
+            Outcomes = File.ReadAllBytes(filename);
+        }
+    }
+}
